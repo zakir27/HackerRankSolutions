beb97a5805066eb15ff6d80bacf89329f6e202a7
zakir27 <84516466+zakir27@users.noreply.github.com>
2021-05-20 19:48:11 +0530

    Add files via upload

---
+++ Practice/C++/Introduction/Input and Output.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    // Initialise array for storing input
+    int arr[3];
+
+    //Receive Input
+    cin>>arr[0]>>arr[1]>>arr[2];
+    
+    //print ans
+    cout<<arr[0]+arr[1]+arr[2];
+    
+    return 0;
+}
