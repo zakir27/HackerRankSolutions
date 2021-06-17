afad74d34b754de14eba921dd654d6cd21eefa01
Mohd Zakir Hussain <84516466+zakir27@users.noreply.github.com>
2021-06-17 20:52:16 +0530

    Update Maps-STL.cpp

--- Practice/C++/STL/Maps-STL.cpp
+++ Practice/C++/STL/Maps-STL.cpp
@@ -10,23 +10,26 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    //Defining Variables
     map<string,int>m;
-    
     int q,qi,y;
     string x;
     map<string,int>::iterator itr;
     
+    //Taking input
     scanf("%d",&q);
     
     while(q--){
+        //Taking Input
         scanf("%d",&qi);
         cin>>x;
         if(qi==1){
+            //Taking Input
             scanf("%d",&y);
             itr=m.find(x);
             
             if(itr==m.end()) m.insert(make_pair(x,y));
-            else m[x]=m[x] +y;
+            else m[x]=m[x]+y;
         }
        
         else if(qi==2) m.erase(x);
